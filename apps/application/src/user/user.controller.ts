3298a2691a33168684742d59080223ca6b73d3df
mati20041 <mat20041@gmail.com>
2023-12-10 18:25:01 +0100

    added user controller

---
+++ apps/application/src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Put } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Controller('user')
+export class UserController {
+  user: User = {
+    id: 1,
+    name: 'John Doe',
+  };
+
+  @Get()
+  getUser() {
+    return this.user;
+  }
+
+  @Put()
+  update(@Body() updateUserDto: UpdateUserDto) {
+    this.user.name = updateUserDto.name;
+    return this.user;
+  }
+}
